2b6fd0abe994052404cd0f2bd1e32150d17731d6
DaShiQiang <derej.kasper@protonmail.com>
2024-07-25 19:29:04 +0100

    Create a computer choice function

---
+++ script.js
@@ -0,0 +1,12 @@
+function getComputerChoice() {
+  let choice = Math.random();
+  if (choice < 0.33) {
+    return "rock";
+  } else if (choice >= 0.33 && choice < 0.66) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
+console.log(getComputerChoice());
+console.log("Hello, World");
